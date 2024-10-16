fd8edb023443e8bd78590f67b28cebba3a404076
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-15 19:45:22 -0700

    set up db connection, connected successfully.  setting up models and controllers.

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 
+// SERUCITY PACKAGES
+import helmet from "helmet";
+import dbConnection from "./dbConfig/index.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8800;
+
+dbConnection();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})

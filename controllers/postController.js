db70d2cdb5c94028de0d27dbf9476a9bb8f17f11
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-23 15:25:13 -0700

    another typo in postModel

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,6 +3,17 @@ export const createPost = async (req, res, next) => {
         const { userId } = req.body.user;
         const { description, image } = req.body;
 
+            if (!description) {
+                next("You must provide a description!")
+                return;
+            }
+
+                const post = await Posts.create({
+                    userId,
+                    description,
+                    image,
+                });
+
     } catch (error) {
         console.log(error);
         res.status(404).json({ message: error.message })
--- models/postModel.js
+++ models/postModel.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from "mongoose";
 const postSchema = new mongoose.Schema(
     {
         userId: { type: Schema.Types.ObjectId, ref: "Users" },
-        descirption: { type: String, required: true },
+        description: { type: String, required: true },
         image: { type: String },
         likes: [{ type: String }],
         comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }], 
